9e71e5e2ed1ba2daad07d0adaf809cbbc94f46ec
git4res <akuzin20@gmail.com>
2023-01-29 09:26:11 +0300

    some styling

--- src/components/common/Pokedex.js
+++ src/components/common/Pokedex.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PokemonCard from "./PokemonCard";
 
 const Pokedex = ({pokemonsArr, exp, isWinner}) => {
-    console.log('pokemonsArr', pokemonsArr)
+    let title;
+    if (isWinner) title = <p className='pokedex-winner'>Winner!</p>
+    else title = <p className='pokedex-loser'>Loser!</p>
+
     return (
         <div className='pokedex'>
+            {title}
             <h1>Pokedex</h1>
             <div>Experience: {exp}</div>
-            <p>{isWinner ? 'Winner!' : 'Loser!'}</p>
             <div className='pokedex-cards'></div>
             {pokemonsArr.map(el => {
                 return <PokemonCard key={el.id} pokemon={el}/>
--- src/components/common/PokemonCard.js
+++ src/components/common/PokemonCard.js
@@ -8,7 +8,10 @@ const PokemonCard = ({pokemon}) => {
     return (
         <div className='pokemon-card'>
             <h1 className='pokemon-card--title'>{pokemon.name}</h1>
-            <img src={imgSrc} alt=""/>
+            <div className='pokemon-card--image'>
+                <img src={imgSrc} alt=""/>
+
+            </div>
             <div className='pokemon-card--data'>Type: {pokemon.type}</div>
             <div className='pokemon-card--data'>Exp: {pokemon.base_experience}</div>
         </div>
